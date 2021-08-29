506c0c892c0869909327e6465d4214878eb2e32b
Akihiko Kusanagi <nagi@nagi-p.com>
2021-08-29 11:31:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Fireworks plugin for Mini Tokyo 3D
+
+Fireworks plugin shows fireworks animations at a specific location on the [Mini Tokyo 3D](https://minitokyo3d.com) map at a scheduled date and time.
+
+![Screenshot](https://nagix.github.io/mt3d-plugin-fireworks/screenshot1.jpg)
+
+Fireworks plugin is used in [Mini Tokyo 3D Live Demo](https://minitokyo3d.com).
+
+## How to Use
+
+First, load the Mini Tokyo 3D and this plugin within the `<head>` element of the HTML file.
+
+```html
+<script src="path/to/mini-tokyo-3d/dist/mini-tokyo-3d.min.js"></script>
+<script src="path/to/mt3d-plugin-fireworks/dist/mt3d-plugin-fireworks.min.js"></script>
+```
+
+Then, create a MiniTokyo3D instance specifying the `plugins` property, which is the array containing the plugin instance returned by `mt3dFireworks()`.
+
+```html
+<div id="map" style="width: 400px; height: 400px;"></div>
+<script>
+    const map = new mt3d.MiniTokyo3D({
+        container: 'map',
+        plugins: [mt3dFireworks()]
+    });
+</script>
+```
+
+## How to Build
+
+The latest version of Node.js is required. Move to the root directory of the plugin, run the following commands, then the plugin scripts will be generated in the `build` directory.
+```bash
+npm install
+npm run build
+```
+
+## License
+
+Fireworks plugin for Mini Tokyo 3D is available under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ rollup.config.js
@@ -0,0 +1,76 @@
+import fs from 'fs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import image from '@rollup/plugin-image';
+import {terser} from 'rollup-plugin-terser';
+import strip from '@rollup/plugin-strip';
+
+const pkg = JSON.parse(fs.readFileSync('package.json'));
+const banner = `/*!
+ * mt3d-plugin-fireworks v${pkg.version}
+ * ${pkg.homepage}
+ * (c) ${new Date().getFullYear()} ${pkg.author}
+ * Released under the ${pkg.license} license
+ */`;
+
+export default [{
+	input: 'src/index.js',
+	output: {
+		name: 'mt3dFireworks',
+		file: `dist/${pkg.name}.js`,
+		format: 'umd',
+		indent: false,
+		sourcemap: true,
+		banner,
+		globals: {
+			'mini-tokyo-3d': 'mt3d'
+		}
+	},
+	external: ['mini-tokyo-3d'],
+	plugins: [
+		resolve(),
+		commonjs(),
+		image()
+	]
+}, {
+	input: 'src/index.js',
+	output: {
+		name: 'mt3dFireworks',
+		file: `dist/${pkg.name}.min.js`,
+		format: 'umd',
+		indent: false,
+		sourcemap: true,
+		banner,
+		globals: {
+			'mini-tokyo-3d': 'mt3d'
+		}
+	},
+	external: ['mini-tokyo-3d'],
+	plugins: [
+		resolve(),
+		commonjs(),
+		image(),
+		terser({
+			compress: {
+				pure_getters: true
+			}
+		}),
+		strip({
+			sourceMap: true
+		})
+	]
+}, {
+	input: 'src/index.js',
+	output: {
+		file: pkg.module,
+		format: 'esm',
+		indent: false,
+		banner
+	},
+	external: ['mini-tokyo-3d'],
+	plugins: [
+		resolve(),
+		commonjs(),
+		image()
+	]
+}];
